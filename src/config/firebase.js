59cd8580ea45c16cbefd48d2adb43a4c05e45296
Davide <davide.sirico@gmail.com>
2023-05-21 00:04:36 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/config/firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-};
+    apiKey: "AIzaSyD2ulTPJWFV4WqIK9qO2B9eAgYcP9ndLSA",
+    authDomain: "discord-1-6b48b.firebaseapp.com",
+    projectId: "discord-1-6b48b",
+    storageBucket: "discord-1-6b48b.appspot.com",
+    messagingSenderId: "348030329480",
+    appId: "1:348030329480:web:f69c925f2540feeed7753d",
+    measurementId: "G-TXSQPRX486"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
